353147fc59bace9b0bf0d0a58c522768a4135fe1
prasannabe2004 <prasannabe2004@gmail.com>
2024-12-30 11:29:19 +0530

    cmake project with debugger in linux

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int, char**){
+    int i;
+    cout << "Hello, from cmake-test!\n";
+    cout << "Enter a number" << endl;
+    cin >> i;
+    cout << "Your number is " << i << endl;
+    return 0;
+}
